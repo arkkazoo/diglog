4e45a6541b373253ba106eb66226f043ba7f3df4
arkkazoo <arkkazoo@gmail.com>
2024-02-22 20:27:55 +0900

    add external api feature

--- server/db.js
+++ server/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
+    ssl: process.env.DISABLE_SSL ? false: true, 
 });
 
 module.exports = pool;
